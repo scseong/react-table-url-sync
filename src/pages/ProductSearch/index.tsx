61e6899fe9089373973c660c9fffe2974ca0f171
scseong <goidtv@naver.com>
2025-06-28 23:54:06 +0900

    feat: products 관련 라우터 및 페이지 컴포넌트 라우팅 설정

---
+++ src/pages/ProductSearch/index.tsx
@@ -0,0 +1,3 @@
+export default function ProductSearch() {
+  return <div>ProductSearch</div>;
+}
---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { default as Categories } from "./Categories";
+export { default as CategoryProducts } from "./CategoryProducts";
+export { default as ProductCreate } from "./ProductCreate";
+export { default as ProductDetail } from "./ProductDetail";
+export { default as ProductEdit } from "./ProductEdit";
+export { default as ProductSearch } from "./ProductSearch";
+export { default as ProductsList } from "./ProductsList";
