74cc1fd2d3be6b38ea0fd49866e488796185f814
scseong <goidtv@naver.com>
2025-07-03 23:53:44 +0900

    feat: 상품 리스트 컴포넌트 구현 및 더미 데이터 fetch 로직 추가

--- src/pages/ProductsList/index.tsx
+++ src/pages/ProductsList/index.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function ProductsList() {
-  return <div>ProductsList</div>;
+  const [, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/mock/products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>로딩 중...</div>;
+
+  return <div>App</div>;
 }
