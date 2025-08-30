5863c28c98976da0534b95673b492c72bdb3ff29
scseong <goidtv@naver.com>
2025-08-30 23:22:35 +0900

    feat: 더미 데이터 활용한 영화 상세정보 데이터 페칭

--- src/pages/MovieDetail/index.tsx
+++ src/pages/MovieDetail/index.tsx
@@ -1,3 +1,26 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import getMovieDetail from "@/apis/getMovieDetail";
+import type { MovieDetail } from "@/types/movies";
+
 export default function MovieDetail() {
+  const [movieDetail, setMovieDetail] = useState<MovieDetail>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchMovieDetail = async () => {
+      try {
+        const res = await getMovieDetail(id || "");
+        setMovieDetail(res);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMovieDetail();
+  }, [id]);
+
+  console.log(movieDetail);
+
   return <div>MovieDetail</div>;
 }
