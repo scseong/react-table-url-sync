4174ad16c752dfa33828dddd59e1176976daf2ab
scseong <goidtv@naver.com>
2025-09-10 22:52:06 +0900

    feat: 영화 리뷰 리스트 페이지 구현

--- src/pages/MovieReviewList/index.tsx
+++ src/pages/MovieReviewList/index.tsx
@@ -1,3 +1,38 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import PageHeader from "@/components/Common/PageHeader";
+import ReviewItem from "@/components/Review/ReviewItem";
+import getMovieReviews from "@/apis/getMovieReviews";
+import type { MovieReview } from "@/types/movies";
+import styles from "./index.module.scss";
+
 export default function MovieReviewList() {
-  return <div>MovieReviewList</div>;
+  const [reviews, setReviews] = useState<MovieReview[]>();
+  const { id: movieId } = useParams();
+
+  useEffect(() => {
+    const fetchMovieReviews = async (movieId?: string) => {
+      if (!movieId) {
+        setReviews([]);
+        return;
+      }
+
+      const res = await getMovieReviews(movieId);
+      setReviews(res);
+    };
+
+    fetchMovieReviews(movieId);
+  }, [movieId]);
+
+  // TODO: movieId null exception
+  if (!movieId) return null;
+
+  return (
+    <main>
+      <PageHeader title="reviews" prevPath={`/movies/${movieId}`} />
+      <section className={styles.reviews}>
+        {reviews?.map((review) => <ReviewItem review={review} />)}
+      </section>
+    </main>
+  );
 }
